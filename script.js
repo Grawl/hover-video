53539c3ca297a7822d3e6e812455a8cc9ec14faf
Даниил Пронин <mail@grawl.ru>
2018-09-26 01:26:36 +1000

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', init())
+function updateTimeout(time = 100) {
+    const data = window['hover-video']
+    clearTimeout(data.timeout)
+    return setTimeout(pauseVideo, time)
+}
+function init() {
+    console.log('init')
+    window['hover-video'] = {}
+    const data = window['hover-video']
+    data.counter = 0
+    data.videoEl = document.querySelector('[hover-play-video]')
+    data.videoEl.play()
+        .then(() => {
+            data.timeout = updateTimeout(1000)
+        })
+        .catch(error => {
+            console.error({ error })
+        })
+}
+function pauseVideo() {
+    const data = window['hover-video']
+    data.videoEl.pause()
+}
+function playVideo() {
+    const data = window['hover-video']
+    data.videoEl.play()
+}
+window['hover-video'].x = window.innerWidth
+document.addEventListener('mousemove', mouseMoveEvent => {
+    // console.log({ mouseMoveEvent })
+    const data = window['hover-video']
+    data.counter++
+    const x = mouseMoveEvent.x
+    if (x > data.x && data.x !== 0) {
+        console.log('right')
+        playVideo()
+        data.timeout = updateTimeout()
+    }
+    data.x = x
+    if (data.counter > 300) {
+        const buttonEl = document.querySelector('[button]')
+        buttonEl.classList.remove('hidden')
+    }
+})
